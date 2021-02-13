c091817cfb49f97c432ed763145daf712a59946d
tobieabel <tobieabel@gmail.com>
2021-02-13 10:22:34 +0000

    Some improvements to segmentation so it adds multiple balls to one mask

--- Advanced Functions.py
+++ Advanced Functions.py
@@ -11,13 +11,45 @@ list2 = [i for i in list]#creates a formal list from the generator
 print(list2)
 list3 = {i:i + 1 for i in list}#creates a dictionary
 print(list3)
-#generator functions - use of Yield key word
+#generator functions - use of Yield key word means instead of running the entire loop in the function
+#, the control is 'yielded' back to the caller when the yield word is encountered.  The state of the function is stored in
+#the 'result' variable, and this can then be passed back to the function with the use of the 'next' keyword
+#which picks up the function where it left of, until the next 'yield' is encountered
 def gen(lst):
     for i in lst:
         print ("next number is ")
-        yield i
+        x = yield i
+    yield x * 2
 
 result = gen(list)
+print(result)
 print(next(result))
+print(result.__next__())#just showing that next is a standard dunnder method of the generator class
+print(next(result))
+print(next(result))
+print(result.send(10))#sends the value 10 to the x variable currently assigned to 'yield'
 
+#Decorators, any funtion which takes as an argument another function, and then modifies that functions behavour
+#The decorator function is declared in the normal function way
+#below is the equivalent of a decorator, a wrapper called as part of another function
+def Decorator_Function(func):
+    def wrapper(*args, **kwargs):#This means the function accepts any number of arguments or keyword argumments
+        print('This is the wrapper talking')
+        func(*args, **kwargs)#use any arguments or keyword arguments passed by the normal function
+        print('This is the decorator talking')
+        return (func(*args, **kwargs))#This ensures any return values from the normal function are repected by the decorator
+    return wrapper
+#create a normal function
+def Normal_Function(word):
+    print (word)
 
+Normal_Function = Decorator_Function(Normal_Function)#assign the variable Normal Function to the decorator, which in turn calls the Normal Function function
+Normal_Function('Hi')#calling Normal Function now actuall calls the variable linked to the Decorator Function
+print(Normal_Function)
+#We can achieve the same thing with the decorator syntax.
+@Decorator_Function
+def Another_Normal_Function(word):
+    print (word)
+    return 10
+result = Another_Normal_Function('Goodbye')#'Goodbye' is an exmaple of passing *arg to the decorator
+print(result)#should print the return value from 'Another_Normal_Function', even though its gone through a decorator
--- Semantic Segmentation.py
+++ Semantic Segmentation.py
@@ -1,80 +1,38 @@
+#Create png mask images for training semantic segmentation.  Creates one class for all balls
+#and puts multiple balls on a single png so there is one png per jpg training image
+
 import os
 import cv2
 import json
 import numpy as np
 
 source_folder = "/Users/tobieabel/Desktop/Ball_Test"
+to_save_folder = "/Users/tobieabel/Desktop/Ball_TestSeg/"
 json_path = "/Users/tobieabel/Desktop/Ball_Test_json/VGG.json"
-count = 0  # Count of total images saved
-file_bbs = {}  # Dictionary containing polygon coordinates for mask
-MASK_WIDTH = 640  # Dimensions should match those of ground truth image
-MASK_HEIGHT = 480
+MASK_WIDTH = 480  # Dimensions should match those of ground truth image
+MASK_HEIGHT = 640
 
 # Read JSON file
 with open(json_path) as f:
     data = json.load(f)
 
-
 # Extract X and Y coordinates if available and update dictionary
-def add_to_dict(data, itr, key, count):
-    try:#pick up here, x_points not being founnd
-        x_points = data[itr]["regions"][count]["shape_attributes"]["all_points_x"]
-        y_points = data[itr]["regions"][count]["shape_attributes"]["all_points_y"]
-    except:
-        print("No BB. Skipping", key)
-        return
-
-    all_points = []
-    for i, x in enumerate(x_points):
-        all_points.append([x, y_points[i]])
-
-    file_bbs[key] = all_points
-
-
 for itr in data:
-    file_name_json = data[itr]["filename"]
-    sub_count = 0  # Contains count of masks for a single ground truth image
+    file_name_png = data[itr]["filename"][:-3] + 'png'
+    all_points = []
+    mask = np.zeros((MASK_WIDTH, MASK_HEIGHT))#black mask
+    for i in range (len(data[itr]["regions"])): #for each region, i.e. each ball
+            x_points = data[itr]["regions"][str(i)]["shape_attributes"]["all_points_x"]
+            y_points = data[itr]["regions"][str(i)]["shape_attributes"]["all_points_y"]
+            for it, x in enumerate(x_points):#for each region go thorugh the x and y points and add them to a list
+                all_points.append([x, y_points[it]])
+            arr = np.array(all_points, 'int32')#turn the list into an nd array
+            cv2.fillPoly(mask, ([arr]), color=(255))#and paste that nd array on the mask
+            arr = 0#clear lists and arrays before processing next region
+            all_points = []
+    cv2.imwrite(os.path.join(to_save_folder, file_name_png), mask)#save the mask to a new image
 
-    if len(data[itr]["regions"]) > 1:#if there is more than one ball
-        for _ in range(len(data[itr]["regions"])):
-            key = file_name_json[:-4] + "*" + str(sub_count + 1)
-            add_to_dict(data, itr, key, sub_count)
-            sub_count += 1
-    else:#if only one ball
-        add_to_dict(data, itr, file_name_json[:-4], 0)
 
-print("\nDict size: ", len(file_bbs))
 
-for file_name in os.listdir(source_folder):
-    to_save_folder = os.path.join(source_folder, file_name[:-4])
-    image_folder = os.path.join(to_save_folder, "images")
-    mask_folder = os.path.join(to_save_folder, "masks")
-    curr_img = os.path.join(source_folder, file_name)
 
-    # make folders and copy image to new location
-    os.mkdir(to_save_folder)
-    os.mkdir(image_folder)
-    os.mkdir(mask_folder)
-    os.rename(curr_img, os.path.join(image_folder, file_name))
 
-# For each entry in dictionary, generate mask and save in corresponding
-# folder
-for itr in file_bbs:
-    num_masks = itr.split("*")
-    to_save_folder = os.path.join(source_folder, num_masks[0])
-    mask_folder = os.path.join(to_save_folder, "masks")
-    mask = np.zeros((MASK_WIDTH, MASK_HEIGHT))
-    try:
-        arr = np.array(file_bbs[itr])
-    except:
-        print("Not found:", itr)
-        continue
-    count += 1
-    cv2.fillPoly(mask, [arr], color=(255))
-
-    if len(num_masks) > 1:
-        cv2.imwrite(os.path.join(mask_folder, itr.replace("*", "_") + ".png"), mask)
-    else:
-        cv2.imwrite(os.path.join(mask_folder, itr + ".png"), mask)
-
-print("Images saved:", count)
